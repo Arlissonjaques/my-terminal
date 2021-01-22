87fae519dac3cece785cd4d255f0a50b028ad07e
Arlisson Jaques <arlissonjaques@gmail.com>
2021-01-21 22:13:57 -0300

    feat(main): added customization code

--- main.sh
+++ main.sh
@@ -12,3 +12,19 @@ echo "Instalando git..."
 apt install git
 
 echo "ok"
+
+echo "Pesonalização do sistema"
+
+PATH_CUSTOMIZATION = "/home/$USER/customization"
+
+mkdir -v -p PATH_CUSTOMIZATION
+
+cd PATH_CUSTOMIZATION && git clone https://github.com/daniruiz/flat-remix
+
+git clone https://github.com/daniruiz/flat-remix-gtk
+
+mkdir -p ~/.icons && mkdir -p ~/.themes
+
+cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
+
+apt install gnome-tweak-tool fonts-hack-ttf -y
